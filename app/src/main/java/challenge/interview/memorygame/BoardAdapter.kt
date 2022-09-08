090d664478f215e7f6c282b87ce1547b2124dfc5
Joel Silva <henriquesa@ipvc.pt>
2022-09-08 17:59:42 +0100

    fixed

--- app/src/main/java/challenge/interview/memorygame/BoardAdapter.kt
+++ app/src/main/java/challenge/interview/memorygame/BoardAdapter.kt
@@ -7,22 +7,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class BoardAdapter(private val context: Context, private val numberCards:Int):
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<BoardAdapter.ViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
        val view = LayoutInflater.from(context).inflate(R.layout.item_card,parent,false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun getItemCount() = numberCards
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(position)
     }
 
-    override fun getItemCount() = numberCards
-
-
-        class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
             fun bind(position: Int){
                 //txt
             }
--- app/src/main/java/challenge/interview/memorygame/MainActivity.kt
+++ app/src/main/java/challenge/interview/memorygame/MainActivity.kt
@@ -2,27 +2,28 @@ package challenge.interview.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recycler: RecyclerView
-    private lateinit var moves: RecyclerView
-    private lateinit var pairs: RecyclerView
+    private lateinit var recyclerBoard: RecyclerView
+    private lateinit var moves: TextView
+    private lateinit var pairs: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler = findViewById(R.id.rvCard)
+        recyclerBoard = findViewById(R.id.rvCard)
         moves = findViewById(R.id.movestv)
         pairs = findViewById(R.id.pairstv)
 
-        recycler.adapter = BoardAdapter(this,8)
-        recycler.setHasFixedSize(true)
-        //adjust later for different sizes
-        recycler.layoutManager = GridLayoutManager(this,2)
+        recyclerBoard.adapter = BoardAdapter(this,8)
+        recyclerBoard.setHasFixedSize(true)
+       // adjust later for different sizes
+        recyclerBoard.layoutManager = GridLayoutManager(this,2)
 
 
 
