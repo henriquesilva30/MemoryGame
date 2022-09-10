0d637c4abb25c4f204f71ca58e1c595941dbea67
Joel Silva <henriquesa@ipvc.pt>
2022-09-10 11:49:50 +0100

    level option check

--- app/src/main/java/challenge/interview/memorygame/MainActivity.kt
+++ app/src/main/java/challenge/interview/memorygame/MainActivity.kt
@@ -5,7 +5,13 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -40,8 +46,26 @@ class MainActivity : AppCompatActivity() {
         pairs = findViewById(R.id.pairstv)
         clRoot = findViewById(R.id.clRoot)
 
-        memoryGame = MemoryGame(boardSize)
+     setupBoard()
+    }
+
+    private fun setupBoard() {
+        when (boardSize){
+            BoardSize.Easy -> {
+                moves.text = "Easy: 4x2"
+                pairs.text = "Pairs: 0/4"
+            }
+            BoardSize.Medium -> {
+                moves.text = "Medium: 6x3"
+                pairs.text = "Pairs: 0/9"
+            }
+            BoardSize.Hard -> {
+                moves.text = "Hard: 7x4"
+                pairs.text = "Pairs: 0/13"
+            }
+        }
         pairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
         adapter = BoardAdapter(this,boardSize,memoryGame.cards,object: BoardAdapter.CardClickListenner{
             override fun onCardClickListenner(position: Int) {
                 //Log.i(TAG,"Card clicked $position")
@@ -50,13 +74,65 @@ class MainActivity : AppCompatActivity() {
 
         })
         recyclerBoard.adapter = adapter
-            recyclerBoard.setHasFixedSize(true)
-       // adjust later for different sizes
+        recyclerBoard.setHasFixedSize(true)
+        // adjust later for different sizes
         recyclerBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth())
-
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?):Boolean{
+            menuInflater.inflate(R.menu.menu_main, menu)
+            return true
+        }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh -> {
+                if (memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()){
+                    showAlertDialog("Quit your current game?",null, View.OnClickListener {
+                        setupBoard()
+                    })
+                } else {
+                setupBoard()
+                }
+                return true
+            }
+            R.id.mi_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showNewSizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+        when (boardSize) {
+            BoardSize.Easy -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.Medium -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.Hard -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose new size", boardSizeView, View.OnClickListener {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.Easy
+                R.id.rbMedium -> BoardSize.Medium
+                else -> BoardSize.Hard
+            }
+
+            setupBoard()
+        })
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK") { _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
 
     private fun updateGameWithFlip(position: Int) {
         //error checking
